dd101d0de6f76694b1055643342b87d919a9f32c
Tyler Rice <tylerrice121@gmail.com>
2022-01-02 16:58:56 -0600

    delete page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom';
 import Kit1 from './pages/kit1/Kit1.js';
-import Kit2 from './pages/kit2/Kit2.js';
-import Kit3 from './pages/kit3/Kit3.js';
-import Kit4 from './pages/kit4/Kit4.js';
 
 function App() {
   return (
